24c88850bf51f3a81ed69ebaab9fa76f3cae044b
EberthCode <eberthrojas98@gmail.com>
2024-03-21 18:19:05 -0500

    Ejercicio 1.4 resuelto

---
+++ exercise_1.4/src/App.jsx
@@ -0,0 +1,62 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </>
+  );
+};
+
+const Header = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        {props.parts[0].name} {props.parts[0].exercises}
+      </p>
+      <p>
+        {props.parts[1].name} {props.parts[1].exercises}
+      </p>
+      <p>
+        {props.parts[2].name} {props.parts[2].exercises}
+      </p>
+    </div>
+  );
+};
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Number of exercises {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}</p>
+    </div>
+  );
+};
+
+export default App;
